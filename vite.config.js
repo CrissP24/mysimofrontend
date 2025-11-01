c196a0f56847c819f020eb3998e4648b82da0df7
CRISTHOPER DAMIAN PINCAY ALAY <128093343+CrissP24@users.noreply.github.com>
2025-10-31 20:37:55 -0500

    Configure base path for production in Vite config
    Set base path for production deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ importante para producción (Vercel)
   server: {
     port: 5173
   }
